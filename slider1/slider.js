411243f05367755cefbe2c713e4763f0ee064fb4
chenyi4 <chenyi4as@163.com>
2017-05-23 21:38:47 +0800

    init

---
+++ slider1/slider.js
@@ -0,0 +1,10 @@
+console.log('sssss');
+var ss = {
+  tt :'sssslll',
+  'skk':'skk'
+}
+for(item in ss){
+  console.log(ss['tt'])
+}
+var imgs = document.getElementById('img').getElementsByTagName('li');
+console.log(imgs[0].style);
