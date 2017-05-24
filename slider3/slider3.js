e17b063c31bc0173f44d208553ed6292166e2d0b
/ </>
2017-05-24 17:05:20 +0800

    improve

--- slider3/slider3.js
+++ slider3/slider3.js
@@ -1,29 +1,72 @@
-
 var alls = document.getElementsByClassName('item');
 var length = alls.length;
-var itemTag = length - 1;
+var itemTag = length - 1;//最后一个元素
 
+console.log(itemTag);
 function set(num,classname,time){
   var start = new Date().getTime();
+  if(!hasClass(num,classname)){
   var className = alls[num].getAttribute('class');
   var newClassName = className +' '+classname;
   alls[num].setAttribute('class',newClassName);
+  }
   var end = new Date().getTime();//计算执行的时间
   return(end - start);
 }
 
-function b(){
-  set(itemTag,'move');
-  set(itemTag-1,'middle');
-  // set(itemTag-1,'move');
-  // set(itemTag-2,'middle');
+function removeClass(num, classname){
+  var className = alls[num].getAttribute('class');
+  var reg = new RegExp('(\\s|^)' + classname + '(\\s|$)');
+  var newClassName = className.replace(reg,' ');
+  alls[num].setAttribute('class',newClassName);
 }
 
-var time = set(itemTag-1, 'right');
-console.log(time);
-setTimeout(b,time);
-//set 一个ckassName到完全可以执行需要一定的时间，如果不设置setTimeout,会发生,setArribute直接把上面一个覆盖掉，
-//一开始把最后第二个div移到右边就完全没有起效
+function hasClass(num,classname){
+  var className = alls[num].getAttribute('class');
+  if(className.indexOf(classname)>-1){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 
-//如何判断一段代码的执行时间
-// b();
+function init(preTag,nextTag,before){
+  set(preTag,'middle');
+  set(nextTag,'right');
+}
+
+function move(preTag,nextTag){
+  set(preTag,'move');
+  set(nextTag,'middle');
+};
+
+function setright(itemTag){
+    removeClass(itemTag,'middle');
+    removeClass(itemTag,'move');
+    set(itemTag,'right');
+}
+  setTimeout("init(itemTag,itemTag-1,itemTag+1)",0);
+  setTimeout("move(itemTag,itemTag-1)",300);
+  setTimeout("setright(itemTag)",1300);
+
+  setTimeout("init(itemTag-1,itemTag-2,itemTag)",1600);
+  setTimeout("move(itemTag-1,itemTag-2)",1900);
+  setTimeout("setright(itemTag-1)",2900);
+
+  setTimeout("init(itemTag-2,itemTag,itemTag-1)",3200);
+  setTimeout("move(itemTag-2,itemTag)",3500);
+  setTimeout("setright(itemTag-2)",4500);
+
+  setTimeout("init(itemTag,itemTag-1,itemTag+1)",4800);
+  setTimeout("move(itemTag,itemTag-1)",5100);
+  setTimeout("setright(itemTag)",6400);
+
+  // setTimeout("init(itemTag-1,itemTag-2,itemTag)",1600);
+  // setTimeout("move(itemTag-1,itemTag-2)",1900);
+  // setTimeout("setright(itemTag-1)",2900);
+  // setTimeout("init(itemTag-1,itemTag-2,itemTag)",1300);
+  // setTimeout("move(itemTag-1,itemTag-2)",1600);
+  //
+  // setTimeout("init(itemTag-1,itemTag-2,itemTag)",1300);
+  // setTimeout("move(itemTag-1,itemTag-2)",1600);
