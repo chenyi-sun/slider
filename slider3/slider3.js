13e7f05225ab777ab34097fa89e554df73aa5c20
/ </>
2017-05-26 08:54:07 +0800

    add

--- slider3/slider3.js
+++ slider3/slider3.js
@@ -2,7 +2,6 @@ var alls = document.getElementsByClassName('item');
 var length = alls.length;
 var itemTag = length - 1;//最后一个元素
 
-console.log(itemTag);
 function set(num,classname,time){
   var start = new Date().getTime();
   if(!hasClass(num,classname)){
@@ -46,21 +45,38 @@ function setright(itemTag){
     removeClass(itemTag,'move');
     set(itemTag,'right');
 }
+
+var i = itemTag;
+var counts = itemTag;
+function start(){
+  counts = counts - 1;
+  if(counts == 0){
+    console.log('结束');
+    return false;
+  }else{
+     start();
+  }
+
   setTimeout("init(itemTag,itemTag-1,itemTag+1)",0);
   setTimeout("move(itemTag,itemTag-1)",300);
   setTimeout("setright(itemTag)",1300);
+  console.log(counts+'===');
 
-  setTimeout("init(itemTag-1,itemTag-2,itemTag)",1600);
-  setTimeout("move(itemTag-1,itemTag-2)",1900);
-  setTimeout("setright(itemTag-1)",2900);
+  // start();
+}
+start();
 
-  setTimeout("init(itemTag-2,itemTag,itemTag-1)",3200);
-  setTimeout("move(itemTag-2,itemTag)",3500);
-  setTimeout("setright(itemTag-2)",4500);
-
-  setTimeout("init(itemTag,itemTag-1,itemTag+1)",4800);
-  setTimeout("move(itemTag,itemTag-1)",5100);
-  setTimeout("setright(itemTag)",6400);
+  // setTimeout("init(itemTag-1,itemTag-2,itemTag)",1600);
+  // setTimeout("move(itemTag-1,itemTag-2)",1900);
+  // setTimeout("setright(itemTag-1)",2900);
+  //
+  // setTimeout("init(itemTag-2,itemTag,itemTag-1)",3200);
+  // setTimeout("move(itemTag-2,itemTag)",3500);
+  // setTimeout("setright(itemTag-2)",4500);
+  //
+  // setTimeout("init(itemTag,itemTag-1,itemTag+1)",4800);
+  // setTimeout("move(itemTag,itemTag-1)",5100);
+  // setTimeout("setright(itemTag)",6400);
 
   // setTimeout("init(itemTag-1,itemTag-2,itemTag)",1600);
   // setTimeout("move(itemTag-1,itemTag-2)",1900);
