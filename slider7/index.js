e10ed2c8ae9a7141b9dc139a08041d917f1cdd73
/ </>
2018-07-26 11:21:42 +0800

    improve

--- slider7/index.js
+++ slider7/index.js
@@ -6,9 +6,10 @@ let setSlider = {
         setTime: 30, //图片切换的时间
         sliderWidth: 300, //图片的宽度
         sliderLength: 0, //图片的长度
-        stopTime: 1200,
+        stopTime: 1200,//图片和图片切换之间的间隔时间
         timePicker: null, //setInterval时间选择器
         timeOutPicker: null, //setTimeout时间选择器
+        isAuto: true
     },
     imgs: [
         './imgs/1.png',
@@ -24,36 +25,54 @@ let setSlider = {
         const self = this;
         const sliders = '<div class="inline-box"><img src="'+self.imgs.join('"/></div><div class="inline-box"><img src="')+'"/></div>';
         this.date.initDom.innerHTML = sliders;
-        this.date.sliderLength = this.imgs.length;
+        this.date.sliderLength = self.imgs.length; 
+        // self.imgs.length;
     },
     setImgsLoop(num){ //设置前面一张图片移动到底部
-        console.log(num);
+        const self = this;
+        let newimgs = [];
+        for(let item in self.imgs){
+            if(item != 0){
+                newimgs.push(self.imgs[item]);
+            }
+        }
+        newimgs.push(self.imgs[0]);
+        self.imgs = newimgs;
+        self.setImgs();
+        self.setImgLeft(0);
     },
     setLeft(num){
         const self = this;
-        let left = num*self.date.sliderWidth;
+        let left = 0;
         self.date.timePicker = setInterval(function(){
-            if(left < self.date.sliderWidth * (self.date.indexNum+1)){
+            if(left < self.date.sliderWidth){
                 left = left + 1;
             }
-            if(left == self.date.sliderWidth * (self.date.indexNum+1)){
+            if(left == self.date.sliderWidth){
                 self.setImgLeft(left);
                 clearInterval(self.date.timePicker);
                 self.date.timePicker = null;
+                let setTime = setTimeout(function(){
+                    self.setImgsLoop(self.date.indexNum);
+                    clearTimeout(setTime);
+                }, self.date.stopTime-1000);
                 self.date.timeOutPicker = setTimeout(function(){
                     if(self.date.indexNum < self.date.sliderLength-2){
                         self.date.indexNum = self.date.indexNum + 1;
                     }
-                    if(num < self.date.sliderLength-2){
+                    if(num == self.date.sliderLength-2 && self.date.isAuto){
+                        num = 0;
+                    }
+                    if((num < self.date.sliderLength-2) && self.date.isAuto){
                         self.setLeft(self.date.indexNum);
                     }
                     clearTimeout(self.date.timeOutPicker);
                     self.date.timeOutPicker = null;
                 }, self.date.stopTime);
+                return false;
             }
             self.setImgLeft(left);
         }, self.setTime);
-
     },
     setImgLeft(left){
         const self = this;
